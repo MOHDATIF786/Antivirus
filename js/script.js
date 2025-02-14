c3bcead5ddfbd58d39bea3bdf1d1a430ec9fffdb
MOHDATIF786 <atifhaashir786@gmailcom>
2025-02-15 03:11:21 +0530

    a new repo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// popup
+setTimeout(() => {
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}, Math.floor(Math.random() * (5000 - 10000) + 10000)); // Random delay between 10-15 seconds
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+// scroll
+document.getElementById("compareButton").addEventListener("click", function () {
+  document
+    .getElementById("compareSection")
+    .scrollIntoView({ behavior: "smooth" });
+});
